55e430a77cf5ffaf845a5af00b022ecc245a85a0
paulwbailey <paul.w.bailey@gmail.com>
2023-04-13 20:41:13 -0600

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ my_fruit_list = my_fruit_list.set_index("Fruit")
 st_ms = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list[my_fruit_list.index.isin(st_ms)])
+streamlit.dataframe(my_fruit_list[my_fruit_list.index.isin(st_ms if not None else my_fruit_list)])
