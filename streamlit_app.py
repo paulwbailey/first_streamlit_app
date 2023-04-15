2a6b2ff5c6bd3d405e970c26991a91ac2cdcefc2
paulwbailey <paul.w.bailey@gmail.com>
2023-04-14 20:08:28 -0600

    TEST

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 def get_fruit_load_list(my_cnx):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
+        my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     return my_cur.fetchall()
 
 
