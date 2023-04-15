06741ddb998f2b492622ab9592db283c23bd45f6
paulwbailey <paul.w.bailey@gmail.com>
2023-04-14 20:09:16 -0600

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def get_fruityvice_data(this_fruit_choice):
 def get_fruit_load_list(my_cnx):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-    return my_cur.fetchall()
+        return my_cur.fetchall()
 
 
 streamlit.title("My Parents New Healthy Diner")
